f664a878571d77ca58863c475060f11eaccb2501
Sushant Suman <Sushant.Suman@lntinfotech.com>
2023-02-16 14:49:17 +0530

    Project Estimation App 1st Commit

---
+++ src/main/java/com/ProjectEstimationApp/ProjectEstimationAppApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectEstimationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEstimationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEstimationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjectEstimationApp/ProjectEstimationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectEstimationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEstimationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
