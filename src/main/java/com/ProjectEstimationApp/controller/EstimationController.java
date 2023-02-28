4f16163453d14b9eb83318276d490dff3977a3b9
10714905 <10714905@ltilpun22001085.lntinfotech.com>
2023-02-28 12:59:52 +0530

    Final UI layouts, estimation.html and questions.html with their respective CSS & JS files.

--- src/main/java/com/ProjectEstimationApp/controller/EstimationController.java
+++ src/main/java/com/ProjectEstimationApp/controller/EstimationController.java
@@ -1,16 +1,28 @@
 package com.ProjectEstimationApp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class EstimationController {
 	
-	//@RequestMapping("/")
-	public String index() {
+	@GetMapping("/estimation")
+	public ModelAndView estimation() {
 		
-		return "index";
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("estimation");
+		
+		return mav;
 	}
 	
-	
+	@GetMapping("/questions")
+	public ModelAndView question() {
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("questions");
+		
+		return mav;
+	}	
 
 }
